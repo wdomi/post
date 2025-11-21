7b36ac5cd3f266aaf24c33782386016d7acb5aa8
wdomi <20965917+wdomi@users.noreply.github.com>
2025-11-21 13:56:26 +0100

    Update rows.js

--- api/rows.js
+++ api/rows.js
@@ -5,9 +5,12 @@ export default async function handler(req, res) {
   const base = "https://api.baserow.io/api/database/rows/table/" + TABLE_ID;
 
   if (req.method === "GET") {
-    const resp = await fetch(base + "/?user_field_names=true&page_size=200", {
-      headers: { Authorization: "Token " + BASEROW_TOKEN }
-    });
+    const resp = await fetch(
+      base + "/?user_field_names=true&page_size=200&filters[processed__boolean]=false",
+      {
+        headers: { Authorization: "Token " + BASEROW_TOKEN }
+      }
+    );
 
     const data = await resp.json();
     return res.status(resp.status).json(data);
