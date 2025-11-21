10b36b1dfeed086a1710d9607b70523c09ca8a06
wdomi <20965917+wdomi@users.noreply.github.com>
2025-11-21 10:30:41 +0100

    Create row.js

---
+++ api/row.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+  const BASEROW_TOKEN = process.env.BASEROW_TOKEN;
+  const TABLE_ID = 745937;
+
+  const { id } = req.query;
+  const base = `https://api.baserow.io/api/database/rows/table/${TABLE_ID}/${id}`;
+
+  if (req.method === "GET") {
+    const resp = await fetch(base + "/?user_field_names=true", {
+      headers: { Authorization: "Token " + BASEROW_TOKEN }
+    });
+
+    const data = await resp.json();
+    return res.status(resp.status).json(data);
+  }
+
+  if (req.method === "PATCH") {
+    const payload = req.body;
+
+    const resp = await fetch(base + "/", {
+      method: "PATCH",
+      headers: {
+        Authorization: "Token " + BASEROW_TOKEN,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
+
+    const data = await resp.json();
+    return res.status(resp.status).json(data);
+  }
+
+  res.status(405).send("Method Not Allowed");
+}
